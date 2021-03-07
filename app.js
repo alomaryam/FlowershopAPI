0143e2eeb977f4fe1d76b913d0428a023d37a5a5
alomaryam <alomaryam@gmail.com>
2021-03-07 23:09:50 +0300

    taskdone

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const flowers = require("./flowers");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.json({ message: "hello world!" });
+});
+
+app.get("/flowers", (req, res) => {
+  res.json(flowers);
+});
+
+app.listen(8001, () => {
+  console.log("The application is running on localhost:8001");
+});
---
+++ flowers.js
@@ -0,0 +1,37 @@
+// const img1 = require("./images/img1.JPG");
+// const img2 = require("./images/img2.JPG");
+// const img3 = require("./images/img3.JPG");
+// const img4 = require("./images/img4.JPG");
+
+const Flowers = [
+  {
+    id: 1,
+    name: "Baby Pink Vase",
+    price: 30,
+    // image: img1,
+    description: "customized vases upon request",
+  },
+  {
+    id: 2,
+    name: "Light Wooden Vase",
+    price: 35,
+    // image: img2,
+    description: "customized vases upon request",
+  },
+  {
+    id: 3,
+    name: "Glass Vase (small)",
+    price: 25,
+    // image: img3,
+    description: "customized vases upon request",
+  },
+  {
+    id: 4,
+    name: "Glass Vase (large)",
+    price: 40,
+    // image: img4,
+    description: "customized vases upon request",
+  },
+];
+
+module.export = Flowers;
