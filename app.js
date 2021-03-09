87d76338a4a5215435b590a840ac58e5a78f5d2a
alomaryam <alomaryam@gmail.com>
2021-03-09 17:54:25 +0300

    deletedone

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const flowers = require("./flowers");
+let flowers = require("./flowers");
 const cors = require("cors");
 
 const app = express();
@@ -12,6 +12,29 @@ app.get("/", (req, res) => {
 
 app.get("/flowers", (req, res) => {
   res.json(flowers);
+});
+
+// app.delete("/flowers/1", (req, res) => {
+//   flowers = flowers.filter((flower) => flower.id !== 1);
+//   console.log("flowers", flowers);
+// });
+
+// app.delete("/flowers/3", (req, res) => {
+//   flowers = flowers.filter((flower) => flower.id !== 3);
+//   console.log("flowers", flowers);
+// });
+
+app.delete("/flowers/:flowerID", (req, res) => {
+  const { flowerID } = req.params;
+  const foundFlower = flowers.find((flower) => flower.id === +flowerID);
+  if (foundFlower) {
+    flowers = flowers.filter((flower) => flower !== foundFlower);
+    // console.log("flowers", flowers);
+    res.status(204).end();
+  } else {
+    res.status(404);
+    res.json({ message: "Flower not found" });
+  }
 });
 
 app.listen(8001, () => {
